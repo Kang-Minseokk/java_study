15228d88498089443c0b042a6ca97ed9df5b33d2
Kang-Minseokk <dreampilot920@naver.com>
2024-08-05 15:28:31 +0900

    Java review 4th day

---
+++ hello/src/EnumExample.java
@@ -0,0 +1,10 @@
+public class EnumExample {
+
+    static Week hope = Week.FRIDAY;
+
+    public static void main(String[] args) {
+        Week today = Week.MONDAY;
+        System.out.println(today);
+        System.out.println(hope);
+    }
+}
